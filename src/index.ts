d5781fde47b18e8fbca6844c2f026fe821c21640
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-12-10 09:57:33 +0700

    docs: make documention open api swagger

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import cors from "cors";
 import helmet from "helmet";
 import compress from "compression";
 import routes from "./routes";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import "./config/database";
 
+const openApiDocument = YAML.load("./docs/openapi.yaml");
 const port: number = Number(process.env.PORT) || 8000;
 const app: Express = express();
 
@@ -13,7 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors()); 
 app.use(helmet());
 app.use(compress());
-app.use("/api/v1", routes);
+app.use('/api/v1', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running on http://localhost:${port}`);
